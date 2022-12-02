5551b9f418f6ddd436992a0c35b7981d3f6d7765
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2022-12-02 12:08:06 -0500

    switch

--- bigO/timeDifProject.rb
+++ bigO/timeDifProject.rb
@@ -1,7 +1,24 @@
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 
 def my_min(list)
-    
+    lowest = list.first
+    list.each do |el|
+        if el < lowest
+            lowest = el
+        end
+    end
+    lowest
 end
 
-my_min(list)  # =>  -5
+
+def my_min(list)
+    list.inject do |acc, el|
+        if el < acc
+            el
+        else
+            acc
+        end
+    end
+end
+
+# p my_min(list)  # =>  -5
